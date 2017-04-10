f9aaa54390949f9533d6e43f1c151c52bdc2dba2
John Nguyen <jnguyen38@gmail.com>
2017-04-10 17:36:05 -0400

    Add files via upload

---
+++ WiFi_Chip.ino
@@ -0,0 +1,21 @@
+void setup() {
+  Serial.begin(9600);
+  while (!Serial); // wait for serial port to connect. Needed for Leonardo only
+  Serial.println("Serial ready");
+//  Serial1.begin(9600);
+  Serial.println("Serial 1 ready");
+}
+
+void loop() {
+  if (Serial.available()) {
+    String str = Serial.readStringUntil('\n');
+    Serial.println("");
+    Serial.println("You sent: " + str);
+  }
+/*   Serial1.println(str);
+  }
+  if (Serial1.available()) {
+    char chr = Serial1.read();
+    if(chr != 0) Serial.print(chr);
+  }*/
+}
